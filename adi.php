7c8865139f181530ea8ddc333d8ad6283c6fe34a
AdiHadian <53321476+AdiHadian@users.noreply.github.com>
2019-07-29 09:58:23 +0700

    Add files via upload

---
+++ adi.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>[Adi Hadian] - [XI RPL 1]</title>
+    </head>
+    <body style="font-family: Arial Narrow;">
+        <img width="250px, height=250px" src=".\wolf.jpg"/>
+        <h1 style="color: Tomato;">Andrew Darwis</h1>
+        <h3 style="color: DodgerBlue;">CEO Kaskus Indonesia</h3>
+        <p>
+            Andrew Darwis (lahir 20 Juli 1979; umur 36 tahun) beliau 
+            adalah pendiri komunitas daring terbesar di Indonesia, Kaskus. 
+            Andrew sekarang menjabat sebagai Chief Technology Officer (CTO) PT Darta Media Indonesia (Kaskus) 
+            sekaligus pemilik (owner) Kaskus Network lewat PT Darta Media Indonesia. 
+            Andrew mendirikan Kaskus pada 6 November 1999. 
+            Bermula dari pengalamannya saat menimba ilmu di universitas Seattle University di Amerika, 
+            Program Studi Multimedia & Web Design, 
+            Art Institute of Seattle Computer Science pada tahun 1999, 
+            pria yang disapa Andrew ini terinspirasi membuat website forum komunitas 
+            yang bisa di bilang menjadi yang terbesar di Indonesia. 
+            Saat itu saya ditugaskan oleh dosen untuk membuat program dari free software, 
+            dari situlah mulai muncul ide membuat website dengan nama Kaskus.
+        </p>
+    </body>
+</html>
